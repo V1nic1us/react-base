f30d0afa0d1638798d813cb838eaafe79745fcec
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-05 16:54:14 -0400

    feat: using color settings

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,4 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import { primaryColor, primaryDarkColor } from '../config/colors';
 
 export default createGlobalStyle`
   * {
@@ -10,7 +11,8 @@ export default createGlobalStyle`
 
   body{
     font-family: sans-serif;
-    background: #ccc;
+    background: ${primaryDarkColor};
+    color: ${primaryDarkColor};
   }
 
   html, body{
@@ -19,10 +21,16 @@ export default createGlobalStyle`
 
   button{
     cursor: pointer;
+    background: ${primaryColor};
+    color: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    font-weight: 700;
   }
 
   a{
     text-decoration: none;
+    color: ${primaryColor};
   }
 
   ul{
