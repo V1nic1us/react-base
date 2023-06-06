87674248d44573839fb7434e7615818bd3080bcd
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-06 08:38:26 -0400

    feat: using a header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './pages/Login/index';
 import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/header.js';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyle />
     </>
