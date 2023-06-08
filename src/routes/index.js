7a0e416d1ecdeb9a519ceae99f395ec8d0f7d6cd
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-08 16:51:44 -0400

    feat: added success and error toast message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
+  toast.success('sucesso', {
+    toastId: 'sucesso',
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
+  toast.error('erro', {
+    toastId: 'erro',
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'light',
+  });
   return (
     <Switch>
       <MyRoute exact path="/" component={Login} />
