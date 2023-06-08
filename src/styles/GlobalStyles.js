913d030d750c98baf557543101756e8391fbf957
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-08 16:53:29 -0400

    feat: importing all colors

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,5 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components';
-import { primaryColor, primaryDarkColor } from '../config/colors';
+import * as colors from '../config/colors';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
   * {
@@ -11,8 +12,8 @@ export default createGlobalStyle`
 
   body{
     font-family: sans-serif;
-    background: ${primaryDarkColor};
-    color: ${primaryDarkColor};
+    background: ${colors.primaryDarkColor};
+    color: ${colors.primaryDarkColor};
   }
 
   html, body{
@@ -21,7 +22,7 @@ export default createGlobalStyle`
 
   button{
     cursor: pointer;
-    background: ${primaryColor};
+    background: ${colors.primaryColor};
     color: white;
     padding: 10px 20px;
     border-radius: 4px;
@@ -30,7 +31,7 @@ export default createGlobalStyle`
 
   a{
     text-decoration: none;
-    color: ${primaryColor};
+    color: ${colors.primaryColor};
   }
 
   ul{
