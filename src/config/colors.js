1beb59bdf93697a50c62136b886a0bd18e8ea22b
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-08 16:48:07 -0400

    feat: new colors

--- src/config/colors.js
+++ src/config/colors.js
@@ -5,3 +5,5 @@ export const successColor = '#0197F6';
 export const infoColor = '#0197F6';
 export const errorColor = '#CF0E0E';
 export const warningColor = '#F2AF29';
+export const blackColor = '#292929';
+export const whiteColor = '#DDDDDD';
