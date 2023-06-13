262cddece972ba5e05a2781bee53f96ceb2e127c
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-12 21:37:32 -0400

    feat: using handleClick to dispatch an event

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
-import axios from '../../services/axios';
 
 export default function Login() {
-  React.useEffect(() => {
-    async function getData() {
-      const response = await axios.get('/alunos');
-      const { data } = response;
-      console.log(data);
-    }
+  const dispatch = useDispatch();
 
-    getData();
-  }, []);
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
 
   return (
     <Container>
@@ -23,6 +23,9 @@ export default function Login() {
       <Paragrafo>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
       </Paragrafo>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
