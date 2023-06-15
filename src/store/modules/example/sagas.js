e63d9557b5ad093f189f85b39c17052b755a9a5d
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-14 22:39:40 -0400

    feat: changing value of clicked button

---
+++ src/store/modules/example/sagas.js
@@ -0,0 +1,25 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import * as actions from './actions';
+import * as types from '../types';
+
+const requisicao = () =>
+  /* eslint-disable */
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+
+function* exampleRequest() {
+  try {
+    yield call(requisicao);
+    toast.success('Sucesso');
+    yield put(actions.clicaBotaoSuccess());
+  } catch (error) {
+    toast.error('Deu erro');
+    yield put(actions.clicaBotaoFailure());
+  }
+}
+
+export default all([takeLatest(types.BOTAO_CLICADO_RESQUEST, exampleRequest)]);
