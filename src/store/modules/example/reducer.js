b73cab3f5912d882efe48ae3c7e3b52c68c0ecb2
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-16 00:00:43 -0400

    style: removed console log

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -7,19 +7,16 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case types.BOTAO_CLICADO_SUCCESS: {
-      console.log('BOTAO_CLICADO_SUCCESS');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
     }
 
     case types.BOTAO_CLICADO_FAILURE: {
-      console.log('BOTAO_CLICADO_FAILURE');
       return state;
     }
 
     case types.BOTAO_CLICADO_RESQUEST: {
-      console.log('BOTAO_CLICADO_RESQUEST');
       return state;
     }
 
